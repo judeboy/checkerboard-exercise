c03befad9f9db453f3ad1acac0a1cf2e8e4e4229
Judah Martin Trimmer <jutr4020@colorado.edu>
2017-10-13 15:09:06 -0600

    gradient

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 
 let body = document.getElementsByTagName('body')[0];
+body.style.background = "linear-gradient(to right,green,teal,blue,indigo,violet, magenta)";
 
   for (let i = 0; i < 63; i++){
     let newDiv = document.createElement('div');
-    let randomHex = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+
+    newDiv.style.border = '1px';
     newDiv.style.float = 'left';
     newDiv.style.width = '11.1%';
     newDiv.style.paddingBottom = '11.1%';
-    newDiv.style.backgroundColor = randomHex;
 
-
+    if (i % 2 === 0) {
+      newDiv.style.backgroundColor = "rgba(173, 33, 99, " + (.02 * i) + ")";
+    } else {
+      newDiv.style.backgroundColor = "rgba(13, 133, 199, " + (.02 * i) + ")";
+    }
     body.appendChild(newDiv);
 }
