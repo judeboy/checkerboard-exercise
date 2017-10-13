3eeda2153ed6967c28920fc3c7815f1f4f71f1f4
Judah Martin Trimmer <jutr4020@colorado.edu>
2017-10-13 11:14:36 -0600

    checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Your JS goes here
+
+var body = document.getElementsByTagName('body')[0];
+
+  for (var i = 0; i < 63; i++){
+    var newDiv = document.createElement('div');
+
+    newDiv.style.float = 'left';
+    newDiv.style.width = '11.1%';
+    newDiv.style.paddingBottom = '11.1%';
+
+    if ( i % 2 === 0){
+      newDiv.style.backgroundColor = 'red';
+    } else {
+      newDiv.style.backgroundColor = 'black';
+    }
+    body.appendChild(newDiv);
+}
