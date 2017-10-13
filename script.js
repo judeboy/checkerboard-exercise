c2d19c2742dbfd8ebb41a8147400469a3366ec73
Judah Martin Trimmer <jutr4020@colorado.edu>
2017-10-13 15:18:23 -0600

    Flashing

--- script.js
+++ script.js
@@ -10,5 +10,13 @@ let body = document.getElementsByTagName('body')[0];
     newDiv.style.backgroundColor = randomHex;
 
 
+
     body.appendChild(newDiv);
 }
+function flash() {
+  for (var i = 0; i < body.children.length; i++) {
+    let randomHex = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    body.children[i].style.backgroundColor = randomHex
+  }
+}
+setInterval(flash, 1);
